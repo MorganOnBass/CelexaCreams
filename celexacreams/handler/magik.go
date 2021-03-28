806c74e149aef0f0c11d54516f2ae303fa504fa3
Morgan MacKechnie <morgan.mackechnie@netprotect.com>
2021-03-28 06:00:19 +0100

    remove superfluous comments

--- celexacreams/handler/magik.go
+++ celexacreams/handler/magik.go
@@ -7,13 +7,6 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// time="2021-03-26T00:17:13Z" level=info msg="[magik]"
-// time="2021-03-26T00:17:13Z" level=info msg="Debug noise!" m.Attachments="[]" m.Content=.magik m.ID=824799114511253554
-// time="2021-03-26T00:17:53Z" level=info msg="[magik]"
-// time="2021-03-26T00:17:53Z" level=info msg="Debug noise!" m.Attachments="[0xc00005a3c0]" m.Content=.magik m.ID=824799285496381500
-// time="2021-03-26T00:24:33Z" level=info msg="[magik https://www.ikea.com/gb/en/images/products/blahaj-soft-toy-shark__0710175_pe727378_s5.jpg?f=xxl]"
-// time="2021-03-26T00:24:33Z" level=info msg="Debug noise!" m.Attachments="[]" m.Content=".magik https://www.ikea.com/gb/en/images/products/blahaj-soft-toy-shark__0710175_pe727378_s5.jpg?f=xxl" m.ID=824800960622231583
-
 // Magik responds to "magik"
 type Magik struct{}
 
@@ -51,6 +44,4 @@ func (h *Magik) Handle(m *discordgo.MessageCreate, c *discordgo.Channel, s *disc
 	}
 	output := mw.GetImageBlob()
 	return "", output, nil
-
-	//return "Not implemented yet, but probably logging to console for Morgan to debug", make([]byte, 0), nil
 }
